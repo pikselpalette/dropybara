df281d66bb9d9de5a7c56e1a6a89c85e19fc1029
Sam Boylett <sam.boylett@piksel.com>
2018-02-05 15:43:28 +0000

    Add drop file to session and use driver for execute_script

--- lib/dropybara.rb
+++ lib/dropybara.rb
@@ -1,10 +1,16 @@
 module Capybara
+  class Session
+    define_method 'drop_file' do |*args, &block|
+      current_scope.send('drop_file', *args, &block)
+    end
+  end
+
   module Node
     module Actions
       def drop_file(locator, *args)
         element_id = "dropybara_input_#{rand(1000)}"
 
-        execute_script <<-JS
+        driver.execute_script <<-JS
           var input = document.createElement('input');
           input.setAttribute('type', 'file');
           input.setAttribute('id', '#{element_id}')
@@ -12,7 +18,7 @@ module Capybara
         JS
 
         attach_file(element_id, *args).tap do
-          execute_script <<-JS
+          driver.execute_script <<-JS
             var input = document.getElementById('#{element_id}');
             var target = document.querySelector('#{locator}');
             var event = new CustomEvent('drop', {
