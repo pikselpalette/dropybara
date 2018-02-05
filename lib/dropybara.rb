a642ee59ce4f8f05bcaf68e22ed8e47339cfbf76
Sam Boylett <sam.boylett@piksel.com>
2018-02-05 15:22:08 +0000

    This isn't javascript land

--- lib/dropybara.rb
+++ lib/dropybara.rb
@@ -2,18 +2,18 @@ module Capybara
   module Node
     module Actions
       def drop_file(locator, *args)
-        ELEMENT_ID = "dropybara_input_#{rand(1000)}"
+        element_id = "dropybara_input_#{rand(1000)}"
 
         execute_script <<-JS
           var input = document.createElement('input');
           input.setAttribute('type', 'file');
-          input.setAttribute('id', '#{ELEMENT_ID}')
+          input.setAttribute('id', '#{element_id}')
           document.body.appendChild(input);
         JS
 
-        attach_file(ELEMENT_ID, *args).tap do
+        attach_file(element_id, *args).tap do
           execute_script <<-JS
-            var input = document.getElementById('#{ELEMENT_ID}');
+            var input = document.getElementById('#{element_id}');
             var target = document.querySelector('#{locator}');
             var event = new CustomEvent('drop', {
               target: target,
