fffd7bd2749a2ceeadb0974585150b3f6f582638
Sam Boylett <sam.boylett@piksel.com>
2018-06-18 16:19:02 +0100

    Add capybara to spec helper

--- spec/unit_spec.rb
+++ spec/unit_spec.rb
@@ -1,11 +1,1 @@
 require_relative '../lib/dropybara.rb'
-
-describe Capybara::Session do
-  before do
-    @session = Capybara::Session.new
-  end
-
-  it 'responds to `drop_file`' do
-    expect(@session).to respond_to(:drop_file)
-  end
-end
