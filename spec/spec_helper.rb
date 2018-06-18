8b07937d9db0347ec5e2ef6d029e0d5eab38c7ae
Sam Boylett <sam.boylett@piksel.com>
2018-06-18 17:17:12 +0100

    try to get headless chrome running on travis

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,7 +11,7 @@ end
 
 Capybara.register_driver :headless_chrome do |app|
   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    chromeOptions: { args: %w[headless disable-gpu] }
+    chromeOptions: { args: %w[no-sandbox headless disable-gpu] }
   )
 
   Capybara::Selenium::Driver.new(
