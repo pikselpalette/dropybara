f677c5ad707e2ed7e16229ee7ee6eb7c6b39d0b4
Sam Boylett <sam.boylett@piksel.com>
2018-06-18 16:17:05 +0100

    use before

--- spec/unit_spec.rb
+++ spec/unit_spec.rb
@@ -1,7 +1,11 @@
 require_relative '../lib/dropybara.rb'
 
 describe Capybara::Session do
+  before do
+    @session = Capybara::Session.new
+  end
+
   it 'responds to `drop_file`' do
-    expect(Capybara::Session.new).to respond_to(:drop_file)
+    expect(@session).to respond_to(:drop_file)
   end
 end
