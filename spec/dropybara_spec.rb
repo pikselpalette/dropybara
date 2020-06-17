0dabb8395f1c74e6322c83a11b185ec30b23a189
Boris Petrov <boris_petrov@live.com>
2020-06-17 10:45:19 +0300

    Make the `drop` event bubble

--- spec/dropybara_spec.rb
+++ spec/dropybara_spec.rb
@@ -11,6 +11,12 @@ describe :drop_file, js: true do
     expect(page).to have_content 'upload.txt'
   end
 
+  it 'can drop a file on a child element and because of bubbling it would still work' do
+    page.drop_file '#sub-dropzone', File.expand_path('spec/files/upload.txt')
+
+    expect(page).to have_content 'upload.txt'
+  end
+
   it 'removes the temporary input it uses' do
     page.drop_file '#dropzone', File.expand_path('spec/files/upload.txt')
 
